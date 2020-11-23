adead136f76b1b00bb70cbd0770b4cd051f469f0
James Hay <jameshay@connectedfreight.com>
2020-11-23 09:30:36 +0800

    Add form fields

--- src/editor/client/components/IndexedPost/IndexedPost.tsx
+++ src/editor/client/components/IndexedPost/IndexedPost.tsx
@@ -14,7 +14,7 @@ export function IndexedPost({ post, id }: Props): ReactElement {
       <Link className="f-b f-s-h2 f-2lh" to={`/post/${id}`}>
         {post.title}
       </Link>
-      <div className="f-s-small f-2elh c-tx-l2">
+      <div className="f-s-sm1 f-2elh c-tx-l2">
         {format(parse(post.created, 'yyyy-MM-dd', new Date()), 'EEEE, do MMMM yyyy')}
       </div>
     </article>
